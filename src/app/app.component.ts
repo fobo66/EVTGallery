626f459dcd1a731c444a4cb8a605a18bee0775b7
Andrey Mukamolow <fobo66@protonmail.com>
2018-04-07 11:25:30 +0300

    Added actual login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,32 @@ import { Image } from './image';
 export class AppComponent {
   title = 'F1 Gallery';
 
+  credentials = new Map([['user', 'password'], ['notadmin', 'notadmin'], ['batman', 'jokerisdead']]);
+
   isLoggedIn = false;
+  isAdmin = false;
 
   private adminLogin = 'admin';
   private adminPassword = 'admin';
 
-  login() {
-    this.isLoggedIn = true;
+  login(login: string, password: string) {
+    this.credentials.forEach((value: string, key: string, map: Map<string, string>) => {
+      if (this.compareCredentials(key, login) && this.compareCredentials(value, password)) {
+        this.isLoggedIn = true;
+      }
+    });
+
+    if (this.compareCredentials(this.adminLogin, login) && this.compareCredentials(this.adminPassword, password)) {
+      this.isLoggedIn = true;
+      this.isAdmin = true;
+    }
   }
 
   signOut() {
     this.isLoggedIn = false;
+  }
+
+  private compareCredentials(expected: string, given: string): boolean {
+    return expected.toLocaleLowerCase().localeCompare(given.toLocaleLowerCase()) === 0;
   }
 }
