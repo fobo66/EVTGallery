bdcaa4d8df719e4d2165e89424b705ac41429705
Andrey Mukamolov <amv@bamboogroup.eu>
2020-05-04 16:42:47 +0300

    Updated Angular version

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { MatDialog } from '@angular/material';
+import { MatDialog } from '@angular/material/dialog';
 import { DeleteConfirmationComponent } from '../delete-confirmation/delete-confirmation.component';
 import { EditDialogComponent } from '../edit-dialog/edit-dialog.component';
 import { Image } from '../image';
